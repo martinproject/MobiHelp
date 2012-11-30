5b9949e7873955671a5fa042b00388a4f91bd839
sumanrajfd <sumanraj@freshdesk.com>
2012-11-30 16:09:53 +0530

    updated to support iosuniversal and iosdevice builds

---
+++ FreshdeskSDK-iosdevice/FreshdeskSDK.framework/Versions/A/Headers/FreshdeskSDK.h
@@ -0,0 +1,10 @@
+//
+//  FreshdeskSDK.h
+//  FreshdeskSDK
+//
+//  Created by Freshdesk (support[at]freshdesk.com)
+//  Copyright (c) 2012 Freshdesk, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FDSupport.h"
